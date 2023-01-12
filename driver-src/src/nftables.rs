9b804a519a0ffd588a195673c54d478fad2d3914
Starrah <starrah@foxmail.com>
2023-01-13 02:00:36 +0800

    Revert f7b55329 "fix: nftables系列指令现在失败会导致程序直接panic，同时任意panic会导致程序直接退出，而不是发生在协程中的panic只中止所在协程。" 这个修改是大半错误的，造成了查验状态、尝试删除的指令由于返回非0而panic，无法实现预期功能。

--- driver-src/src/nftables.rs
+++ driver-src/src/nftables.rs
@@ -1,6 +1,6 @@
 /// 这是driver部分的源代码。不得改动！
 use std::io::Write;
-use std::process::{Command, Output, Stdio};
+use std::process::{Command, Stdio};
 
 use anyhow::Result;
 
@@ -9,12 +9,12 @@ pub struct NftGuard {}
 impl NftGuard {
     pub fn new(all: bool) -> NftGuard {
         // 如果没添加ip规则，就添加
-        if run_command("ip rule list fwmark 84").stdout.len() == 0 {
-            run_command("ip rule add fwmark 84 lookup 84");
+        if get_command("ip rule list fwmark 84").output().unwrap().stdout.len() == 0 {
+            get_command("ip rule add fwmark 84 lookup 84").output().unwrap();
             println!("已配置ip rule规则")
         }
-        if run_command("ip route list table 84").stdout.len() == 0 {
-            run_command("ip route add 0.0.0.0/0 dev lo table 84");
+        if get_command("ip route list table 84").output().unwrap().stdout.len() == 0 {
+            get_command("ip route add 0.0.0.0/0 dev lo table 84").output().unwrap();
             println!("已配置ip route规则")
         }
         let nft_conf = include_str!("nftables.conf");
@@ -24,7 +24,6 @@ impl NftGuard {
 
         let mut child = get_command("nft -f -").stdin(Stdio::piped()).spawn().unwrap();
         child.stdin.as_mut().unwrap().write_all(patched_conf.as_bytes()).unwrap();
-        if !child.wait().unwrap().success() { panic!("配置nft规则失败！") }
         println!("已配置nftables规则，类型为{}({})", name, match_cidr);
         NftGuard {}
     }
@@ -46,31 +45,25 @@ fn get_command(command: &str) -> Command {
     command
 }
 
-fn run_command(command: &str) -> Output {
-    let output = get_command(command).output().unwrap();
-    if !output.status.success() { panic!("执行命令{}失败！", command) }
-    output
-}
-
 fn clear_nft() {
-    run_command("nft delete table ip exp4");
+    get_command("nft delete table ip exp4").output().unwrap();
 }
 
 // 连同ip rule一起清掉
 pub fn clear_all() {
     clear_nft();
     // 清除ip配置
-    while run_command("ip rule list fwmark 84").stdout.len() > 0 {
-        run_command("ip rule delete fwmark 84");
+    while get_command("ip rule list fwmark 84").output().unwrap().stdout.len() > 0 {
+        get_command("ip rule delete fwmark 84").output().unwrap();
     }
-    run_command("ip route flush table 84");
+    get_command("ip route flush table 84").output().unwrap();
     println!("所有配置(nftables, ip rule, ip route)已清除！")
 }
 
 fn print_one(command_str: &str) -> Result<()> {
     println!("$ {}", command_str);
-    let output = get_command(command_str).output()?;
-    println!("{}", std::str::from_utf8(&output.stdout)?);
+    let mut command = get_command(command_str);
+    println!("{}", std::str::from_utf8(&command.output()?.stdout)?);
     Ok(())
 }
 
